7c7e392f5c753082cc769f1084370a748656de7d
Daniel Horton <dancrhorton@gmail.com>
2024-12-10 20:45:19 -0500

    combine day 5

--- aoc_2024/day5.py
+++ aoc_2024/day5.py
@@ -3,7 +3,7 @@ import networkx as nx
 from aoc_2024.utils import by_line, get_day_and_input, log_part_1, log_part_2, log_start
 
 
-def part_1(data):
+def part_1_2(data):
     graph = nx.DiGraph()
     paths = []
     for line in by_line(data):
@@ -14,46 +14,29 @@ def part_1(data):
         elif "," in line:
             paths.append([int(s.strip()) for s in line.split(",")])
 
-    count = 0
-
-    for path in paths:
-        if nx.is_path(graph, path):
-            count += path[int((len(path)) / 2)]
-
-    return count
-
-
-def part_2(data):
-    graph = nx.DiGraph()
-    paths = []
-    for line in by_line(data):
-        if "|" in line:
-            u, v = line.split("|")
-            graph.add_edge(int(u), int(v))
-
-        elif "," in line:
-            paths.append([int(s.strip()) for s in line.split(",")])
-
-    count = 0
+    count_1 = 0
+    count_2 = 0
     for path in paths:
         sub = graph.subgraph(path)
-        if not nx.is_path(sub, path):
+        if nx.is_path(sub, path):
+            count_1 += path[int((len(path)) / 2)]
+
+        else:
             try:
                 p = list(nx.topological_sort(sub))
-                count += p[int((len(p)) / 2)]
+                count_2 += p[int((len(p)) / 2)]
             except:  # noqa: S112,E722
                 continue
 
-    return count
+    return count_1, count_2
 
 
 def run():
     d, f = get_day_and_input(__file__)
 
     log_start(d)
-    part1 = part_1(f)
+    part1, part2 = part_1_2(f)
     log_part_1(part1)
-    part2 = part_2(f)
     log_part_2(part2)
 
 
