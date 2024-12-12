c7d75544bfc75db6bc6c2312a6885069e72ac3c6
Daniel Horton <dancrhorton@gmail.com>
2024-12-11 20:28:37 -0500

    day 11

---
+++ aoc_2024/day11.py
@@ -0,0 +1,46 @@
+import math
+from collections import defaultdict
+from functools import lru_cache
+
+from aoc_2024.utils import get_day_and_input, log_part_1, log_part_2, log_start
+
+
+@lru_cache(maxsize=None)
+def get_keys(k):
+    if k == 0:
+        return (1,)
+    digits = math.floor(math.log10(k)) + 1
+    if digits % 2 == 0:
+        e = 10 ** (digits // 2)
+        return divmod(k, e)
+    return (k * 2024,)
+
+
+def part_1(data, blinks):
+    stones = {}
+    for num in data.strip().split(" "):
+        stones[int(num)] = 1
+
+    for _ in range(blinks):
+        new = defaultdict(int)
+        for k, v in stones.items():
+            keys = get_keys(k)
+            for key in keys:
+                new[key] += v
+
+        stones = new
+
+    return sum(stones.values())
+
+
+def run():
+    d, f = get_day_and_input(__file__)
+    log_start(d)
+    part1 = part_1(f, 25)
+    log_part_1(part1)
+    part2 = part_1(f, 75)
+    log_part_2(part2)
+
+
+if __name__ == "__main__":
+    run()
--- aoc_2024/main.py
+++ aoc_2024/main.py
@@ -2,7 +2,7 @@ import time
 
 from loguru import logger
 
-from aoc_2024 import day1, day2, day3, day4, day5, day6, day7, day8, day9, day10
+from aoc_2024 import day1, day2, day3, day4, day5, day6, day7, day8, day9, day10, day11
 
 
 def main():
@@ -17,6 +17,7 @@ def main():
     timeit(day8.run)
     timeit(day9.run)
     timeit(day10.run)
+    timeit(day11.run)
 
 
 def timeit(fn):
